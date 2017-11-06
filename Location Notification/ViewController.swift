1c3dc126db9b96744a83861b23814e3f21d9e9cf
Amir J <amir.jahan@gmail.com>
2017-11-06 11:11:59 -0800

    first commit

--- Location Notification/ViewController.swift
+++ Location Notification/ViewController.swift
@@ -1,25 +1,127 @@
-//
-//  ViewController.swift
-//  Location Notification
-//
-//  Created by Amir on 11/5/17.
-//  Copyright © 2017 Cinard Digital Media Institute. All rights reserved.
-//
-
 import UIKit
+import UserNotifications
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UNUserNotificationCenterDelegate, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
+    var region : CLCircularRegion!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        getUserPermission();
+        
+        
+
+        locationManager.delegate = self
+        
+        
+        // must
+        locationManager.requestWhenInUseAuthorization()
+
+        // Simple repeatable way
+        let notCenter = CLLocationCoordinate2D(latitude: 49, longitude: -123)
+        let notRegion = CLCircularRegion(center: notCenter, radius: 2000.0, identifier: "lhl")
+        notRegion.notifyOnEntry = true
+        notRegion.notifyOnExit = false
+        let trigger = UNLocationNotificationTrigger(region: notRegion, repeats: false)
+        showNotification(inpTrigger: trigger)
+        
+        
+        
+        
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+        locationManager.distanceFilter = 50;
+        let center = CLLocationCoordinate2D(latitude: 49,
+                                            longitude: -123)
+        region = CLCircularRegion(center: center,
+                                  radius: 1000.0,
+                                  identifier: "lhl")
+
+        locationManager.startMonitoring(for: region)
+        
+        if !CLLocationManager.isMonitoringAvailable(for: CLCircularRegion.self) {
+            print("Can't use this for monitorinh")
+        }
+        locationManager.startUpdatingLocation()
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func locationManager(_ manager: CLLocationManager,
+                         didUpdateLocations locations: [CLLocation]) {
+        print(locations)
     }
-
-
+    
+    func locationManager(_ manager: CLLocationManager, monitoringDidFailFor region: CLRegion?, withError error: Error) {
+        print("failed to monitor")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didStartMonitoringFor region: CLRegion) {
+        print("began monitoring for \(region)")
+    }
+    
+    func locationManager(_ manager: CLLocationManager,
+                         didEnterRegion region: CLRegion) {
+        print("Geofence Entered")
+        let trigger = UNLocationNotificationTrigger(region: region,
+                                      repeats: false)
+        showNotification(inpTrigger: trigger)
+    }
+    
+    func locationManager(_ manager: CLLocationManager,
+                         didExitRegion region: CLRegion) {
+        print("Geofence Exited")
+    }
+    
+    func showNotification (inpTrigger: UNLocationNotificationTrigger) {
+        let locationTrigger = inpTrigger
+        
+        let locationContent = UNMutableNotificationContent()
+        locationContent.title = "Location Based Event"
+        locationContent.body = "This should happen within LHL!"
+        locationContent.sound = UNNotificationSound.default()
+        
+        // Creating notification request
+        let request = UNNotificationRequest(identifier: "myId",
+                                            content: locationContent,
+                                            trigger: locationTrigger)
+        
+        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+        UNUserNotificationCenter.current().add(request) {(error) in
+            if let error = error {
+                print("error with request: \(error)")
+            }
+        }
+    }
+    
+    func getUserPermission()
+    {
+        // grant access by user to receve notifications
+        UNUserNotificationCenter
+            .current()
+            .requestAuthorization(options: [.alert, .sound])
+            {
+                (accepted, error) in
+                var alert : UIAlertController;
+                
+                if !accepted
+                {
+                    alert = UIAlertController(title: "Denied",
+                                              message: "Notification access denied.",
+                                              preferredStyle: UIAlertControllerStyle.alert);
+                }
+                else
+                {
+                    alert = UIAlertController(title: "Granted",
+                                              message: "Notification access granted.",
+                                              preferredStyle: UIAlertControllerStyle.alert);
+                }
+                alert.addAction(UIAlertAction(title: "Ok",
+                                              style: UIAlertActionStyle.default,
+                                              handler: nil))
+                
+                self.present(alert, animated: true, completion: nil)
+        }
+    }
 }
-
